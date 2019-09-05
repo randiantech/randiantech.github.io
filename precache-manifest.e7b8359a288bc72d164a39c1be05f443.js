0f016ea7a9f5b403dc3e08e7e97864d5b8d8fd12
Juan Carlos Cancela <jccancela@aetion.com>
2019-09-05 20:08:20 -0300

    updated site

---
+++ precache-manifest.e7b8359a288bc72d164a39c1be05f443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81946f49172cbe7b2e88a1c811e6846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f93b2e5ccc397e4dfd2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "9f93b2e5ccc397e4dfd2",
+    "url": "/static/js/main.56a70bac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
