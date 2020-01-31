f2e05675bb47dfcbb2ee35c61d5099ee526981a6
Juan Carlos Cancela <bak@Juans-MacBook-Pro-3.local>
2020-01-31 00:26:16 -0300

    updated assets

---
+++ precache-manifest.5fe7b9c1171e7781815db2a47f4b7a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff80917de24133d7a7a7c4cceb0fbe94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbeee74df4cb1be0743a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "fbeee74df4cb1be0743a",
+    "url": "/static/js/main.b24bbb8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
