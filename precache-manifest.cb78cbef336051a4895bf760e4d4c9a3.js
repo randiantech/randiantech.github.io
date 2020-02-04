0c4b7b801903010b3affcbd51c330be8430f76a8
Juan Carlos Cancela <bak@Juans-MacBook-Pro-3.local>
2020-02-04 00:16:30 -0300

    updated site

---
+++ precache-manifest.cb78cbef336051a4895bf760e4d4c9a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ee4138962483db07359ad1ae02b7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb3f6ddaf65f40f5662",
+    "url": "/static/css/main.35c62e5c.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "7cb3f6ddaf65f40f5662",
+    "url": "/static/js/main.1229addb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
